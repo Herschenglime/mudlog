f552504cb0708c7588f6b3f995188b3c5085b635
Herschenglime <herschenglime@gmail.com>
2023-03-04 16:08:28 -0500

    navbar cleaned up a bit

---
+++ src/lib/components/NavItem.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import Card from '$lib/components/Card.svelte';
+  export let highlightOnHover = true;
+  export let link: string, background: string = 'var(--color-lighter)';
+</script>
+
+<div style:background>
+  <a href={link}
+  style:text-decoration="none"
+  style:color="inherit">
+    <slot />
+  </a>
+</div>
+
+<style>
+</style>
