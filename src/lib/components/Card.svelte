4b1577ce73cf681e5753860f02a4c940bdf0f627
Herschenglime <herschenglime@gmail.com>
2023-03-03 20:24:04 -0500

    theming implemented

---
+++ src/lib/components/Card.svelte
@@ -0,0 +1,14 @@
+
+<div class="card">
+  <slot/>
+</div>
+
+<style>
+.card {
+   border-radius: 10px;
+   background: var(--color-light);
+   padding: 5px;
+   margin: 5px;
+
+ }
+</style>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+ import { theme } from '$lib/stores'
+ import ThemeSwitch from '$lib/components/ThemeSwitch.svelte'
+</script>
+
+<svelte:head>
+  <meta name="color-scheme" content={$theme == 'system' ? 'light dark' :
+  $theme}/> <link rel="stylesheet" href={`/theme/${$theme}.css`} />
+</svelte:head>
+
+<ThemeSwitch/>
+<slot />
