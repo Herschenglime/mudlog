1b1a4bf78671a1486cf7e5f1542e1a24c87f0c0a
Herschenglime <herschenglime@gmail.com>
2023-03-04 22:56:05 -0500

    fixed date in entry form

--- src/lib/components/EntryForm.svelte
+++ src/lib/components/EntryForm.svelte
@@ -1,36 +1,28 @@
-  <script lang="ts">
-    import { createForm } from "svelte-forms-lib";
-    export let entry:entry;
+<script lang="ts">
+  import { createForm } from 'svelte-forms-lib';
+  export let entry: entry;
 
-    const { form, handleChange, handleSubmit } = createForm({
-      initialValues: {
-        title: "",
-        body: ""
-      },
-      onSubmit: values => {
-       entry.title = values.title;
-       entry.body = values.body;
-      }
-    });
+  const { form, handleChange, handleSubmit } = createForm({
+    initialValues: {
+      title: '',
+      body: ''
+    },
+    onSubmit: (values) => {
+      entry.title = values.title;
+      entry.body = values.body;
+    }
+  });
 
-   $: entry = {title: $form.title, body: $form.body}
-  </script>
+  $: entry.title = $form.title;
+  $: entry.body = $form.body;
+</script>
 
-  <form on:submit={handleSubmit}>
-    <label for="title">Title</label>
-    <input
-      id="title"
-      name="title"
-      on:change={handleChange}
-      bind:value={$form.title}/>
+<form on:submit={handleSubmit}>
+  <label for="title">Title</label>
+  <input id="title" name="title" on:change={handleChange} bind:value={$form.title} />
 
-    <label for="body">Body</label>
-    <textarea
-      id="body"
-      name="body"
-      on:change={handleChange}
-      bind:value={$form.body}
-    />
+  <label for="body">Body</label>
+  <textarea id="body" name="body" on:change={handleChange} bind:value={$form.body} />
 
-    <button type="submit">Submit</button>
-  </form>
+  <button type="submit">Submit</button>
+</form>
